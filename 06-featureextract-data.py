52285acf882e77e2058178ae702bef5120ba66e6
cgenevier <cgenevier@gmail.com>
2026-01-30 11:18:05 -0700

    Update 06-featureextract-data.py

--- 06-featureextract-data.py
+++ 06-featureextract-data.py
@@ -1,3 +1,5 @@
-# Channel Selection/Dropping
-# Temporal Window Selection (Cropping)
-# Per-Epoch Normalization
+# Full: All 64 channels
+# Moderate: F3, F1, Fz, F2, F4, FC3, FC1, FCz, FC2, FC4, C3, C1, Cz, C2, C4, CP3, CP1, CP2, and CP4 (Si-Mohammed et al., 2020)
+# Minimal: Only FCz and Cz (Torres et al., 2018)
+
+# Also normalize epochs for ML
